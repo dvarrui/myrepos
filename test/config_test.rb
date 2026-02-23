0df032d4a6bff901f6d4fb73eb68695082bba671
David Vargas <dvarrui@protonmail.com>
2026-02-23 22:46:34 +0000

    add test config err

--- test/config_test.rb
+++ test/config_test.rb
@@ -4,19 +4,22 @@ require "test_helper"
 
 class ConfigTest < Test::Unit::TestCase
   def setup
-    dirpath = File.join(File.dirname(__FILE__), "files")
-    @config = Config.new(dirpath)
+    dirpath = File.join(File.dirname(__FILE__), "files", "config.ok")
+    @config_ok = Config.new(dirpath)
+    dirpath = File.join(File.dirname(__FILE__), "files", "config.err")
+    @config_err = Config.new(dirpath)
   end
 
   test "Config attributes" do
-    abs_dirpath = File.absolute_path(@config.dirpath)
-    assert_equal abs_dirpath, @config.abs_dirpath
+    abs_dirpath = File.absolute_path(@config_ok.dirpath)
+    assert_equal abs_dirpath, @config_ok.abs_dirpath
 
     configdir = File.join(abs_dirpath, "mygit.d")
-    assert_equal configdir, @config.configdir
+    assert_equal configdir, @config_ok.configdir
   end
 
   test "Config is_ok?" do
-    assert @config.is_ok?
+    assert @config_ok.is_ok?
+    assert !@config_err.is_ok?
   end
 end
