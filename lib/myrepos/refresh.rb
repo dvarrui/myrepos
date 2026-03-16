831d0088ad0ddcb9d65726325d6a3c842dc2f8f9
David Vargas <dvarrui@protonmail.com>
2026-03-16 09:10:39 +0000

    update refresh

--- lib/myrepos/refresh.rb
+++ lib/myrepos/refresh.rb
@@ -4,19 +4,19 @@ module MyRepos
   class Refresh
     def initialize(dirpath)
       @config = MyRepos::Config.new(dirpath)
+      @config.load
       @pastel = Pastel.new
     end
 
     def call
-      puts "==> [myrepos] Refreshing..."
-      @config.load
-      puts @config.data
+      puts "==> [myrepos] Refreshing local repos..."
       find_local_repos
     end
 
     def find_local_repos
       local_repo_paths = []
       @config.data[:local_repos].each do |basedir|
+        puts "==> [myrepos] Looking into: #{@pastel.green(basedir)}"
         filepaths = Dir.glob(File.join(basedir, "*/**/.git"))
         local_repo_paths += filepaths.map { File.dirname(_1) }
       end
