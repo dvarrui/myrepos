509e8fc66c3933c5687e7deb9240c2b62b792bfe
David Vargas <dvarrui@protonmail.com>
2026-03-16 11:32:42 +0000

    update refresh

--- lib/myrepos/refresh.rb
+++ lib/myrepos/refresh.rb
@@ -7,30 +7,34 @@ module MyRepos
       @config = MyRepos::Config.new(dirpath)
       @config.load
       @pastel = Pastel.new
-      @local_repos = []
+      @local_repos = get_local_repos
     end
 
     def call
-      puts "==> [myrepos] Refreshing local repos..."
-      find_local_repos
+      puts "==> [refresh] local repos..."
+      @local_repos.each do
+        puts @pastel.yellow.bold("==> [refresh] pull #{it.basedir}...")
+        it.pull
+      end
     end
 
     def show
-      @local_repos.each { puts it.to_s }
+      puts "==> [refresh] List:"
+      @local_repos.each do
+        puts "    " + @pastel.blue.bold(it.to_s)
+      end
     end
 
     private
 
-    def find_local_repos
-      local_repo_paths = []
+    def get_local_repos
+      local_paths = []
       @config.data[:local_repos].each do |basedir|
-        puts "==> [myrepos] Searching within: #{@pastel.green(basedir)}"
+        puts "==> [refresh] Searching within: #{@pastel.green(basedir)}"
         filepaths = Dir.glob(File.join(basedir, "*/**/.git"))
-        local_repo_paths += filepaths.map { File.dirname(_1) }
+        local_paths += filepaths.map { File.dirname(_1) }
       end
-      local_repo_paths.each do |localrepopath|
-        @local_repos << LocalRepo.new(localrepopath)
-      end
+      local_paths.map { |path| LocalRepo.new(path)}
     end
   end
 end
--- lib/myrepos/repo/local.rb
+++ lib/myrepos/repo/local.rb
@@ -1,14 +1,29 @@
+require "pastel"
+
 module MyRepos
   class LocalRepo
+    attr_reader :basedir
+
     def initialize(basedir)
+      @pastel = Pastel.new
       @basedir = basedir
+      @pull_state = false
     end
 
     def to_s
-      "LocalRepo: #{@basedir}"
+      "LocalRepo: #{@basedir} [pull=#{@pull_state}]"
     end
 
     def pull
+      Dir.chdir(@basedir) do
+        begin
+          @pull_state = system("git pull")
+        rescue => e
+          puts @pastel.red.bold("==> [LocalRepo.pull] Error: #{e.message}")
+          @pull_state = false
+        end
+      end
+      @pull_state
     end
   end
 end
