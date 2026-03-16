262b179fa8ab535e08ef20a6cf8ddf7b15f9befe
David Vargas <dvarrui@protonmail.com>
2026-03-16 08:50:21 +0000

    class into module

--- lib/myrepos/refresh.rb
+++ lib/myrepos/refresh.rb
@@ -1,24 +1,26 @@
 require_relative "config"
 
-class Refresh
-  def initialize(dirpath)
-    @config = Config.new(dirpath)
-    @pastel = Pastel.new
-  end
-
-  def call
-    puts "==> [myrepos] Refreshing..."
-    @config.load
-    puts @config.data
-    find_local_repos
-  end
-
-  def find_local_repos
-    local_repo_paths = []
-    @config.data[:local_repos].each do |basedir|
-      filepaths = Dir.glob(File.join(basedir, "*/**/.git"))
-      local_repo_paths += filepaths.map { File.dirname(_1) }
+module MyRepos
+  class Refresh
+    def initialize(dirpath)
+      @config = MyRepos::Config.new(dirpath)
+      @pastel = Pastel.new
     end
-    puts local_repo_paths.join("\n")
+
+    def call
+      puts "==> [myrepos] Refreshing..."
+      @config.load
+      puts @config.data
+      find_local_repos
+    end
+
+    def find_local_repos
+      local_repo_paths = []
+      @config.data[:local_repos].each do |basedir|
+        filepaths = Dir.glob(File.join(basedir, "*/**/.git"))
+        local_repo_paths += filepaths.map { File.dirname(_1) }
+      end
+      puts local_repo_paths.join("\n")
+    end
   end
 end
